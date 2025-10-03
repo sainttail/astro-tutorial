2c70174a4d5ccd865aea81f39558b56fab715ba1
Eakawat Tantamjarik <eakawat2232@gmail.com>
2025-10-03 22:05:36 +0700

    Add interactions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://kaleidoscopic-praline-97f6b5.netlify.app/"
+  site: "https://kaleidoscopic-praline-97f6b5.netlify.app/",
+  integrations: [preact()]
 });
