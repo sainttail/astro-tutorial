9f4704089feeb776bf9af014e88ba81e17e98d7a
Eakawat Tantamjarik <eakawat2232@gmail.com>
2025-10-03 21:06:56 +0700

    Add dynamic page routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://kaleidoscopic-praline-97f6b5.netlify.app/"
+});
